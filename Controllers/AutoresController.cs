4652527c9b51659c622da0afa66413dde4870ace
Andrea <andrea@example.com>
2026-07-24 02:21:19 +0000

    Agrega modulo Autores: modelo, controlador y vista con Bootstrap

---
+++ Controllers/AutoresController.cs
@@ -0,0 +1,22 @@
+using Biblioteca.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biblioteca.Controllers
+{
+    public class AutoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            var autores = new List<Autor>
+            {
+                new Autor { Id = 1, Nombre = "Gabriel", Apellido = "García Márquez", Nacionalidad = "Colombiana", FechaNacimiento = new DateTime(1927, 3, 6), Activo = true },
+                new Autor { Id = 2, Nombre = "Isabel", Apellido = "Allende", Nacionalidad = "Chilena", FechaNacimiento = new DateTime(1942, 8, 2), Activo = true },
+                new Autor { Id = 3, Nombre = "Mario", Apellido = "Vargas Llosa", Nacionalidad = "Peruana", FechaNacimiento = new DateTime(1936, 3, 28), Activo = false },
+                new Autor { Id = 4, Nombre = "Jorge Luis", Apellido = "Borges", Nacionalidad = "Argentina", FechaNacimiento = new DateTime(1899, 8, 24), Activo = false },
+                new Autor { Id = 5, Nombre = "Claudia", Apellido = "Lars", Nacionalidad = "Salvadoreña", FechaNacimiento = new DateTime(1899, 12, 20), Activo = true }
+            };
+
+            return View(autores);
+        }
+    }
+}
---
+++ Models/Autor.cs
@@ -0,0 +1,12 @@
+namespace Biblioteca.Models
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Nacionalidad { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public bool Activo { get; set; }
+    }
+}
